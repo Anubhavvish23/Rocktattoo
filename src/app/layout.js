f1ca65dede1705b3aec8716a5df9ba5d46513fe2
ANubhavvish23 <anubhavsanjay01@gmail.com>
2025-02-11 21:11:38 +0530

    Updated page.js and layout.js with recent changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,31 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Rock Tattoo',
   description: 'Rock Tattoo',
-  metadataBase: new URL('https://your-domain.com'),
-  icons: {
-    icon: [
-      { url: '/logo.jpg' },
-      { url: '/logo.jpg', sizes: '16x16', type: 'image/jpg' },
-      { url: '/logo.jpg', sizes: '32x32', type: 'image/jpg' }
-    ],
-    apple: [
-      { url: '/logo.jpg', sizes: '180x180', type: 'image/jpg' }
-    ]
-  },
-  openGraph: {
-    title: 'Rock Tattoo',
-    description: 'Rock Tattoo',
-    images: [{
-      url: '/logo.jpg',
-      width: 1200,
-      height: 630,
-      alt: 'Rock Tattoo',
-    }]
-  }
-}
-
-export const viewport = {
-  themeColor: '#40aae3'
+  image: '/logo.jpg',
 }
 
 export default function RootLayout({ children }) {
